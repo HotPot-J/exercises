62634904b8073272bd41e1f56c43394f70807e3c
HotPot-J <807126916@qq.com>
2020-02-22 22:37:18 +0800

    二叉树求和

---
+++ exercises_day56/day35/test.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+/*
+给定二叉搜索树的根结点 root，返回 L 和 R（含）之间的所有结点的值的和。
+二叉搜索树保证具有唯一的值。
+
+示例 1：
+输入：root = [10, 5, 15, 3, 7, null, 18], L = 7, R = 15
+输出：32
+示例 2：
+输入：root = [10, 5, 15, 3, 7, 13, 18, 1, null, 6], L = 6, R = 10
+输出：23
+来源：力扣（LeetCode）
+链接：https ://leetcode-cn.com/problems/range-sum-of-bst
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+
+ typedef struct TreeNode {
+     int val;
+     struct TreeNode *left;
+     struct TreeNode *right;
+ }Node;
+
+ int rangeSumBST(struct TreeNode* root, int L, int R){
+	 int sum = 0;
+	 if (!root){
+		 return 0;
+	 }
+	 if (root->val >= L&&root->val <= R){
+		 sum += root->val;
+	 }
+	 sum += rangeSumBST(root->left, L, R);
+	 sum += rangeSumBST(root->right, L, R);
+
+	 return sum;
+ }
+
