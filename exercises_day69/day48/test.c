66fe9050a9a7c6656f568ef869cb45394fe070ad
HotPot-J <807126916@qq.com>
2020-03-22 23:50:25 +0800

    1.寻找第K大数字 思路：排序 简单

---
+++ exercises_day69/day48/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+/*
+215. 数组中的第K个最大元素
+在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+示例 1:
+输入: [3,2,1,5,6,4] 和 k = 2
+输出: 5
+示例 2:
+输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+输出: 4
+*/
+int cmp(const void* a, const void* b){
+	return *(int*)a < *(int*)b;
+}
+int findKthLargest(int* nums, int numsSize, int k){
+	qsort(nums, numsSize, sizeof(int), cmp);
+	return nums[k - 1];
+}
